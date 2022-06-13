c04014b3a4e695127971fd29d6b114d9a1ec1e25
MastromauroUnB <211029263@aluno.unb.br>
2022-06-13 15:04:04 -0300

    Até o exerecício 10 feito (sem comentários)

--- Lista de exercícios/src/Exercicio7.java
+++ Lista de exercícios/src/Exercicio7.java
@@ -1,9 +1,33 @@
-
+import java.util.Scanner;
 public class Exercicio7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 String nome;
+		 int qtde;
+		 double precoUnitario, total, totalDescontado;
+		 
+		 Scanner leitor = new Scanner(System.in);
+		 
+		 System.out.println("Digite o nome do produto");
+		 nome = leitor.next();
+		 System.out.println("Digite a quantidade comprada");
+		 qtde = leitor.nextInt();
+		 System.out.println("Digite o preco unitario");
+		 precoUnitario = leitor.nextDouble();
+		 total = precoUnitario * qtde;
+		 if(qtde <= 5) {
+			 totalDescontado = total - total*0.02;
+		 }
+		 else if(qtde <= 10) {
+			 totalDescontado = total - total*0.03;
+		 }
+		 else {
+			 totalDescontado = total - total*0.05;
+		 }
+		 System.out.println("O preco total de " + qtde + " unidades de " + nome + " sem desconto é de " + total);
+		 System.out.println("O preco total de " + qtde + " unidades de " + nome + " com desconto é de " + totalDescontado);
+		 leitor.close();
 	}
 
 }
